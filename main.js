6cbf0e50103128c1ab25c10b363e2f4d345113e5
Divyanshu <lucky1nautiyal@gmail.com>
2026-04-06 11:46:47 +0530

    Implemented creation of matrix by inputting rows columns

---
+++ main.js
@@ -0,0 +1,34 @@
+const inputRCBtn = document.getElementById("inputRCButton");
+const createMatrixBtn = document.getElementById("createMatrix");
+let rowVal, colVal;
+
+inputRCBtn.addEventListener("click", inputRowCol)
+function inputRowCol()
+{
+    rowVal = document.getElementById("row").value;
+    colVal = document.getElementById("column").value
+}
+
+createMatrixBtn.addEventListener("click", makeMatrix);
+
+function makeMatrix()
+{
+    const matrixContainer = document.getElementById("matrixContainer");
+    matrixContainer.innerHTML = "";  
+    console.log("btn working");
+
+    for (let i = 1; i <= rowVal; i++)
+    {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row-div");
+        matrixContainer.appendChild(rowDiv);
+        for(let j = 1; j <= colVal; j++)
+            {
+                const cell = document.createElement("input");
+                cell.type = "number";
+                cell.id = `cell-${i}-${j}`;
+                cell.value = 31;
+                rowDiv.appendChild(cell);
+            }
+        }
+}
