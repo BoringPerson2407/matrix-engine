55f83633ec95698f81d70137fdf4f4e71826fc4b
Divyanshu <lucky1nautiyal@gmail.com>
2026-04-07 22:53:44 +0530

    Implemented matrix output, save, and addition functions

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const inputRCBtn = document.getElementById("inputRCButton");
 const createMatrixBtn = document.getElementById("createMatrix");
+const testBtn = document.createElement("button");
+testBtn.innerHTML = "Test";
+
+const saveABtn = document.getElementById("saveA");
+const saveBBtn = document.getElementById("saveB");
+const addBtn = document.getElementById("addBtn");
+
 let rowVal = 2;
 let colVal = 2;
+let matrixA = [];
+let matrixB = [];
 
 inputRCBtn.addEventListener("click", inputRowCol)
 function inputRowCol()
@@ -28,9 +37,61 @@ function makeMatrix()
                 const cell = document.createElement("input");
                 cell.type = "text";
                 cell.id = `cell-${i}-${j}`;
-                cell.value = 31;
+                cell.value = `${i}${j}`;
                 // cell.value = cell.value.replace(/[^0-9.-]/g, ""); Implement this later on
                 rowDiv.appendChild(cell);
             }
         }
+    
+    // matrixContainer.appendChild(testBtn);
+}
+
+// testBtn.addEventListener("click", () => {
+//     const matrixVal = getMatrix();
+//     console.log(matrixVal);
+// });
+
+function getMatrix()
+{
+    const matrix = [];
+    for(let i = 1; i <= rowVal; i++)
+    {
+        const row = [];
+        for(let j = 1; j <= colVal; j++)
+        {
+            const cell = document.getElementById(`cell-${i}-${j}`);
+            row.push(parseFloat(cell.value));
+        }
+        matrix.push(row);
+    }
+    
+    return matrix;  
+}
+
+
+saveABtn.addEventListener("click", () => {
+    matrixA = getMatrix();
+    console.log(matrixA);
+})
+
+saveBBtn.addEventListener("click", () => {
+    matrixB = getMatrix();
+    console.log(matrixB);
+})
+
+addBtn.addEventListener("click", addMatrices);
+
+function addMatrices()
+{
+    const result = [];
+    for (let i = 0; i < matrixA.length; i++)
+    {
+        const row = [];
+        for (let j = 0; j < matrixA[i].length; j++)
+        {
+            row.push(matrixA[i][j] + matrixB[i][j]);
+        }
+        result.push(row);
+    }
+    console.log(result);
 }
